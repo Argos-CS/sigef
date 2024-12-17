7ffda580ef4952fee375f0fdf4a19af6ad34a74d
Ice Man <140863320+Argos-CS@users.noreply.github.com>
2024-12-17 01:55:56 -0300

    Delete sigef-main directory

--- sigef-main/src/integrations/supabase/types/enums.ts
+++
@@ -1,6 +0,0 @@
-import { Database } from './database-types';
-
-export type CategoriaLevel = Database['public']['Enums']['categoria_nivel'];
-export type ContaTipo = Database['public']['Enums']['conta_tipo'];
-export type UserRole = Database['public']['Enums']['user_role'];
-export type UserStatus = Database['public']['Enums']['user_status'];
--- sigef-main/src/integrations/supabase/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Database } from './database-types';
-export type { Json } from './json';
-export * from './enums';
-export * from './composite-types';
-export * from './models';
