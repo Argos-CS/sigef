7137debf22b5ceae84a4c306fc607d110248060b
Ice Man <140863320+Argos-CS@users.noreply.github.com>
2025-01-03 00:12:54 +0000

    vite.config

--- vite.config_BKP.ts
+++ vite.config_BKP.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { vitePlugin as remix } from '@remix-run/dev'; // Import the Remix Vite plugin
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -11,7 +10,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    remix(), // Add the Remix plugin
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
